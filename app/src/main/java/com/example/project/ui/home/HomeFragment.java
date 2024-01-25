71281c2b98cee68120c0e7f250c71ab625f0fc1c
Archie Agarwal <archieagarwal2001@gmail.com>
2024-01-25 15:25:21 +0530

    Geoapify Library

--- app/src/main/java/com/example/project/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project/ui/home/HomeFragment.java
@@ -1,32 +1,25 @@
 package com.example.project.ui.home;
-import com.geoapify.*;
-import com.geoapify.exceptions.GeoapifyException;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.ImageView;
-import android.widget.Button;
-import android.widget.Toast;
-
 import com.example.project.R;
 import com.example.project.databinding.FragmentHomeBinding;
+import com.geoapify.*;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment {
