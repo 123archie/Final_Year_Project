582e63433cf0ff6fad7914554c1f34fa5dce5c5c
Archie Agarwal <archieagarwal2001@gmail.com>
2024-02-20 19:13:19 +0530

    Project Commit 3

--- app/src/main/java/com/example/project/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/project/ui/home/HomeFragment.java
@@ -2,11 +2,10 @@ package com.example.project.ui.home;
 
 import static android.app.appsearch.AppSearchResult.RESULT_OK;
 
-import static com.google.android.libraries.places.widget.AutocompleteActivity.RESULT_ERROR;
-
 import android.content.Intent;
-import android.icu.util.IndianCalendar;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,34 +15,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.android.volley.toolbox.HttpResponse;
 import com.example.project.R;
 import com.example.project.databinding.FragmentHomeBinding;
 import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
-import com.google.android.libraries.places.api.model.AutocompleteSessionToken;
 import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.widget.Autocomplete;
 import com.google.android.libraries.places.widget.AutocompleteActivity;
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Objects;
 
 
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
 import okhttp3.Response;
 
 public class HomeFragment extends Fragment {
@@ -55,14 +51,17 @@ public class HomeFragment extends Fragment {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         Response response=null;
-        EditText name, passID, price;
+        EditText name, passID, price, editName, editPass;
         Spinner spin;
         ImageView fingerprint;
         Button btnsubmit;
         ArrayList<String> spinnerList = new ArrayList<>();
+        Drawable img=getContext().getDrawable(R.mipmap.warning);
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         name = binding.editName.findViewById(R.id.name);
+        editName=binding.editName.findViewById(R.id.edit_name);
         passID = binding.editPass.findViewById(R.id.passId);
+        editPass=binding.editPass.findViewById(R.id.edit_pass);
         spin = binding.spinner.findViewById(R.id.spinner);
         start = binding.editStart.findViewById(R.id.edit_start);
         dest = binding.editDest.findViewById(R.id.edit_dest);
@@ -79,17 +78,32 @@ public class HomeFragment extends Fragment {
         start.setFocusable(false);
         dest.setFocusable(false);
         start.setOnClickListener(new View.OnClickListener() {
-            @Override
+//            @Override
             public void onClick(View v) {
-                List<Place.Field> fieldlist= Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME
-                       );
-                Log.d("FieldList: ", "FieldList: "+fieldlist);
-                Intent intent=new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldlist).build(requireContext());
-                startActivityForResult(intent,100);
-                Log.d("startactive", "StartActivity:");
+//                List<Place.Field> fieldlist= Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG, Place.Field.NAME
+//                       );
+//                Log.d("FieldList: ", "FieldList: "+fieldlist);
+//                Intent intent=new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldlist).build(requireContext());
+//                startActivityForResult(intent,100);
+//                Log.d("startactive", "StartActivity:");
+//
+//            }
 
-            }
-        });
+                OkHttpClient client = new OkHttpClient();
+
+                Request request = new Request.Builder()
+                        .url("https://geoapify-address-autocomplete.p.rapidapi.com/v1/geocode/autocomplete?text=Polizeigasse&type=street&lang=de&limit=2&filter=countrycode%3Ade%2Ces%2Cfr&bias=proximity%3A10.485306%2C48.852565")
+                        .get()
+                        .addHeader("X-RapidAPI-Key", "84743f608emsh304f1d6aff5f6afp1d8849jsnfdcf72505aa4")
+                        .addHeader("X-RapidAPI-Host", "geoapify-address-autocomplete.p.rapidapi.com")
+                        .build();
+
+                try {
+                    com.squareup.okhttp.Response response = client.newCall(request).execute();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }});
         dest.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -100,6 +114,35 @@ public class HomeFragment extends Fragment {
                 startActivityForResult(intent,100);
                 Log.d("startactive", "StartActivity:");
 
+            }
+        });
+        double start_latitude=0.0;
+        double start_longitude=0.0;
+        double end_latitude=0.0;
+        double end_longitude=0.0;
+        int dist=calculateDistance();
+        price.setText("Rs. "+(dist*2));
+        price.setCursorVisible(false);
+        price.setFocusable(false);
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                if(editName.getText().toString()==""){
+                    name.setCompoundDrawables(null, null, img, null);
+                }
+                else if(editPass.getText().length()==0){
+
+                }
+                else if(start.getText().length()==0){
+
+                }else if(dest.getText().length()==0){
+
+                }else if(price.getText().length()==0){
+
+                }
+             else{
+                Toast.makeText(getContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+             }
             }
         });
         View root = binding.getRoot();
@@ -121,7 +164,10 @@ public class HomeFragment extends Fragment {
 
         }
     }
+    public int calculateDistance(){
 
+        return 0;
+    }
     @Override
         public void onDestroyView() {
             super.onDestroyView();
